bbc7b979d6a106b1132250e25eee407aceec5003
CentOS <centos@centos8.linuxvmimages.local>
2023-12-13 15:10:17 -0500

    first commit

---
+++ docker-install.sh
@@ -0,0 +1,4 @@
+#https://linuxhint.com/install-use-docker-centos-8/
+
+sudo dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
+sudo dnf install docker-ce --nobest --allowerasing -y
