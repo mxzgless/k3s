d4dbd19d045b15441fd92259310c0061da2ce3be
CentOS <centos@centos8.linuxvmimages.local>
2023-12-14 10:55:36 -0500

    new

--- 20-config.sh
+++ 20-config.sh
@@ -2,7 +2,7 @@ set -e -v
 
 sudo groupadd -f k3s
 sudo usermod -aG k3s $USER
-sudo chown centos:k3s /etc/rancher/k3s/k3s.yaml
+sudo chown $USER:k3s /etc/rancher/k3s/k3s.yaml
 mkdir -p $HOME/.kube
 sudo cp /etc/rancher/k3s/k3s.yaml $HOME/.kube/config
 sudo chown $USER $HOME/.kube/config
