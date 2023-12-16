16be25ec37c3d8b7dbb37edd32a3e9de06fbc02b
CentOS <centos@centos8.linuxvmimages.local>
2023-12-16 17:15:23 -0500

    fix

--- site-shart/hello-world.sh
+++ site-shart/hello-world.sh
@@ -1,5 +1,5 @@
-kubectl create namespace hello
-kubectl create configmap hello-world -n hello --from-file index.html
+kubectl create namespace hello3
+kubectl create configmap hello-world -n hello3 --from-file index.html
 kubectl apply -f deployment.yaml
 kubectl apply -f service.yaml
 cat ingress.yaml | envsubst | kubectl apply -f -
