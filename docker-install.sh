f7c3248bd078aecc37e699e38c89ccb861ac0f83
mxzgless <152324497+mxzgless@users.noreply.github.com>
2024-04-07 14:37:49 +0300

    add ubuntu

--- docker-install.sh
+++ docker-install.sh
@@ -3,4 +3,8 @@
 sudo dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
 sudo dnf install docker-ce --nobest --allowerasing -y
 
+
+#for ubuntu 20 04
+#https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-on-ubuntu-20-04
+
 #fix permition
