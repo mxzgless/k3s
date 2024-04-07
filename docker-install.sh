a6f376c626c8630387fc157995e9e9d20f0088d7
mxzgless <152324497+mxzgless@users.noreply.github.com>
2024-04-07 14:44:00 +0300

    Update docker-install.sh
    add helm  and kubectl

--- docker-install.sh
+++ docker-install.sh
@@ -1,10 +1,15 @@
-#https://linuxhint.com/install-use-docker-centos-8/
-
+#docker for centos  dohttps://linuxhint.com/install-use-docker-centos-8/
 sudo dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
 sudo dnf install docker-ce --nobest --allowerasing -y
 
 
-#for ubuntu 20 04
+#docker for ubuntu 20 04
 #https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-on-ubuntu-20-04
 
-#fix permition
+#install helm
+#https://helm.sh/ru/docs/intro/install/
+
+
+#install kubectl
+#https://pwittrock.github.io/docs/tasks/tools/install-kubectl/
+
